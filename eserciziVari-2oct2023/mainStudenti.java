addcb651d99cda9fe2de7dc82be32d1e0b718c91
Erica4School <146210186+Erica4School@users.noreply.github.com>
2023-10-02 15:32:37 +0200

    Add files via upload

---
+++ eserciziVari-2oct2023/mainStudenti.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class mainStudenti {
+
+	public static void main(String[] args) {
+	
+		/*Random ra = new Random();
+		tavola[] pippo = new tavola[10];
+		for (tavola erica: pippo) {
+			erica = new tavola(ra.nextInt(5), ra.nextInt(14));
+			System.out.println(erica.toString());
+		}
+		
+		
+		/*student[] pippo = new student[4];
+		//creo valori randomici 
+		Random ra = new Random();
+//popolo il mio array
+		for (int i=0; i<pippo.length; i++) {
+	
+		//creare l istanza di studente
+		pippo[i] = new student();
+		pippo[i].numero= i+1;
+		pippo[i].crediti= ra.nextInt(5)+10; //numeri generati tra 10 e 14
+		pippo[i].voto = ra.nextInt(80)+30;
+	}
+	
+//matricola 3 - FOR EACH
+	int matricolaDaCercare = 3;
+	for (student x : pippo) {
+		if (x.numero==3) {
+			System.out.println(x.info());
+		}
+	}*/
+
+}}
+
---
+++ eserciziVari-2oct2023/stu.java
@@ -0,0 +1,34 @@
+
+public class stu {
+	private String nome;
+	private int pt;
+/*int numero, crediti,voto;*/
+	//int larghezza, altezza;
+	//costruttore
+/*	tavola(int larghezza, int altezza){
+		this.larghezza = larghezza;
+		this.altezza= altezza;
+	}*/
+	//@Override
+	/*public String toString() {
+		return "tavola [larghezza=" + larghezza + ", altezza=" + altezza + "]";
+	}*/
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getPt() {
+		return pt;
+	}
+	public void setPt(int pt) {
+		this.pt = pt;
+	}
+	
+	
+//String info() {
+//	return "studente numero "+ numero + "numero crediti: "+ crediti + " voto "+voto;
+//}
+}
+
